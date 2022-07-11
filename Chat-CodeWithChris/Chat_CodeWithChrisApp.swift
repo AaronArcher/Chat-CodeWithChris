abbc253a94b8cc8df3d16d497c31fb5bde1b39e6
Aaron Johncock <aaron.johncock@hotmail.co.uk>
2022-07-11 19:59:14 +0100

    Initial Commit

---
+++ Chat-CodeWithChris/Chat_CodeWithChrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_CodeWithChrisApp.swift
+//  Chat-CodeWithChris
+//
+//  Created by Aaron Johncock on 11/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Chat_CodeWithChrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
